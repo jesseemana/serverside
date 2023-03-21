9b3de02a3ac920a7f97eefe2b4a7d7fd4c621168
Jesse Emana <jesseemana@gmail.com>
2023-03-21 16:03:59 +0200

    modified commas

--- server.js
+++ server.js
@@ -1,68 +1,70 @@
-require('dotenv').config();
-require('colors');
-require('express-async-errors');
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
-const cors = require("cors");
-const multer = require("multer");
-const helmet = require("helmet");
-const path = require("path");
-const {fileURLToPath} = require("url");
-const morgan = require('morgan');
-const connectDB = require('./config/connectDB');
-const {registerUser} = require('./controllers/auth');
-const verifyJWT = require('./middleware/auth');
-const {createPost} = require('./controllers/posts');
+require('dotenv').config()
+require('colors')
+require('express-async-errors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+const cors = require("cors")
+const multer = require("multer")
+const helmet = require("helmet")
+const path = require("path")
+const {fileURLToPath} = require("url")
+const morgan = require('morgan')
+const connectDB = require('./config/connectDB')
+const {registerUser} = require('./controllers/auth')
+const verifyJWT = require('./middleware/auth')
+const {createPost} = require('./controllers/posts')
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000
 
-connectDB();
+connectDB()
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-const app = express();
+const app = express()
 
 
 // MIDDLEWARE 
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
-app.use(morgan("common"));
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+
+app.use("/assets", express.static(path.join(__dirname, "public/assets")))
 
 
 // MULTER SETUP FOR FILE UPLOADING
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'public/assets');
+        cb(null, 'public/assets')
     },
     filename: function(req, file, cb) {
-        cb(null, file.originalname);
+        cb(null, file.originalname)
     }
-});
+})
 
-const upload = multer({storage});
+const upload = multer({storage})
 
 
 // USER PROFILE SETUP 
 // WON'T BE IN ROUTES FOLDER BECAUSE WE WANT TO USE MULTER UPLOAD IN INDEX 
-app.post('/auth/register', upload.single('picture'), registerUser);
-app.post('/posts/create', verifyJWT, upload.single('picture'), createPost);
+app.post('/auth/register', upload.single('picture'), registerUser)
+app.post('/posts/create', verifyJWT, upload.single('picture'), createPost)
 
 
 // ROUTES 
-app.use('/auth', require('./routes/auth'));
-app.use('/users', require('./routes/user'));
-app.use('/posts', require('./routes/posts'));
+app.use('/auth', require('./routes/auth'))
+app.use('/users', require('./routes/user'))
+app.use('/posts', require('./routes/posts'))
 
 
 mongoose.connection.once('open', () => {
-    console.log(`Database connected...`.cyan.underline);
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`.cyan.underline));
-});  
+    console.log(`Database connected...`.cyan.underline)
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`.cyan.underline))
+})      
